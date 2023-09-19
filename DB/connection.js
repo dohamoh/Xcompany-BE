22146ffb6d0e5b26206461816cb06cb4341db3d4
dohamoh <doh.mohammed@gmail.com>
2023-09-19 17:32:47 +0300

    d3

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connection = async ()=>{
   mongoose.set('strictQuery', true);
-    return await mongoose.connect("mongodb+srv://ziad:00241300@cluster0.bxnbg.mongodb.net/Company")
+    return await mongoose.connect("mongodb+srv://dohmohammed:doha2001@cluster0.io5stmr.mongodb.net/Xcompany")
     .then(()=> console.log(`connected on ...... `))
     .catch(err=>console.log(`fail to connect `))
 }
